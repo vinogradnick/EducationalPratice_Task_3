0e41eea41af7ada699dc1757693d86e24111a89f
vinogradovnick32 <vinogradovnick32@gmail.com>
2018-05-23 20:14:30 +0500

    first commit

--- EducationalPratice_Task_3/Program.cs
+++ EducationalPratice_Task_3/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace EducationalPratice_Task_3
 {
+    /// <summary>
+    /// Дана действительная квадратичная матрица порядка 9
+    /// Получить целочисленную квадратную матрицу того же порядка,
+    ///  в которой элемент равен еденице, 
+    ///     если соответствующий ему элемент исходной матрицы больше элемента, 
+    ///         расположенного в его строке на главной диагонали,
+    ///  и равен нулю в противном случае.
+    /// </summary>
     class Program
     {
+        static Random random = new Random();
         static void Main(string[] args)
         {
+           int
+        }
+
+        static int function(int x)
+        {
+            int y;
+            if (x < 0)
+                y = Math.Abs(x);
+            else if (x < 2)
+                y = x;
+            else
+            {
+                if (x > 2 && x < 3)
+                {
+                    y = 1;
+                }
+                else
+                    y = x - (x + 1);
+            }
         }
     }
 }
